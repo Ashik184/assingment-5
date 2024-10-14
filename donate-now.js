3d67473f12b605e4720f74b2d43a0234034ac9f3
Ashik184 <anjamularefin@gmail.com>
2024-10-14 18:54:14 +0600

    few left

--- donate-now.js
+++ donate-now.js
@@ -30,7 +30,7 @@ function change(show,hide,green,white){
 
 
 
-
+// noakhali card
 document.getElementById('btn-donate-now')
     .addEventListener('click',function(event){
         event.preventDefault();
@@ -42,7 +42,9 @@ document.getElementById('btn-donate-now')
         if(isNaN(noakhali)){
             alert("invalid number");
         }
-        
+        const modal = document.getElementById('my_modal_10');
+        modal.showModal();
+
         const totalBalance = parseFloat(addMoney) + parseFloat(noakhali); 
         
         document.getElementById('noakhali-balance').innerText = totalBalance;
@@ -57,6 +59,83 @@ document.getElementById('btn-donate-now')
         // history section 
         const p = document.createElement('p');
         p.innerText = `${addMoney} Taka is  Donate for Flood at Noakhali, Bangladesh`;
+       
+
+        // should be a common function
+        document.getElementById('history-section').appendChild(p);
+
+    })
+
+
+
+    //feni card
+    document.getElementById('btn-donate-now2')
+    .addEventListener('click',function(event){
+        event.preventDefault();
+        
+        const addMoney = document.getElementById('input-add-money2').value;
+        
+        const feni = document.getElementById('feni-balance').innerText;
+
+        if(isNaN(feni)){
+            alert("invalid number");
+        }
+        const modal = document.getElementById('my_modal_10');
+        modal.showModal();
+
+        const totalBalance = parseFloat(addMoney) + parseFloat(feni); 
+        
+        document.getElementById('feni-balance').innerText = totalBalance;
+
+        // total amount calculation
+        const accountBalance = document.getElementById('account-balance').innerText;
+        
+        const totalAmount = parseFloat(accountBalance) - parseFloat(addMoney);
+        
+        document.getElementById('account-balance').innerText = totalAmount;
+        
+        // history section 
+        const p = document.createElement('p');
+        p.innerText = `${addMoney} Donate for Flood Relief in Feni,Bangladesh`;
+       
+
+        // should be a common function
+        document.getElementById('history-section').appendChild(p);
+
+    })    
+
+
+    // Qouta card 
+    document.getElementById('btn-donate-now3')
+    .addEventListener('click',function(event){
+        event.preventDefault();
+        
+        const addMoney = document.getElementById('input-add-money3').value;
+        
+        const qouta = document.getElementById('qouta-balance').innerText;
+
+        const modal = document.getElementById('my_modal_10');
+        modal.showModal();
+
+
+        if(isNaN(qouta)){
+            alert("invalid number");
+        }
+        
+        const totalBalance = parseFloat(addMoney) + parseFloat(qouta); 
+        
+        document.getElementById('qouta-balance').innerText = totalBalance;
+
+        // total amount calculation
+        const accountBalance = document.getElementById('account-balance').innerText;
+        
+        const totalAmount = parseFloat(accountBalance) - parseFloat(addMoney);
+        
+        document.getElementById('account-balance').innerText = totalAmount;
+        
+        // history section 
+        const p = document.createElement('p');
+        p.innerText = `${addMoney} Aid for Injured in the Quota Movement`;
        
 
         // should be a common function
